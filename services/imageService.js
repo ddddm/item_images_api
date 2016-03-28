93cfb560c759dd47961c4cac7763367a118ecf19
ddddm <m1290d@gmail.com>
2016-03-29 03:19:30 +0500

    imageServer now can create folders for different thumbnail sizes

--- services/imageService.js
+++ services/imageService.js
@@ -1,42 +1,116 @@
+'use strict';
+
 var Promise = require('bluebird');
 var gm = require('gm');
-// Promise.promisifyAll(gm.prototype);
 var fs = Promise.promisifyAll(require('fs'));
 
 var imageFolder = 'images/';
 var cacheFolder = 'cache/';
 
 module.exports = {
-  jpg: function(fileName, size) {
+    jpg: function (fileName, size) {
 
-    var width = size.split('x')[0] !== ''? parseInt(size.split('x')[0]) : null;
-    var height = size.split('x')[1] !== ''? parseInt(size.split('x')[1]) : null;
+        var normalizedSizeObject = normalizeSizeObject(size);
+        var outputFormat = 'jpg';
+        var path;
 
-    return new Promise(function(resolve, reject) {
+        var shoudBeResized = !!(normalizedSizeObject.width || normalizedSizeObject.height);
 
-      var _fileName = size? cacheFolder + size + '/' + fileName : imageFolder + fileName;
+        // if should be resized:
+        // 1. check cache size folder
+        // 2. check cached file
+        // 3. create new one if needed
 
-      return fs.statAsync(_fileName)
-        .catch(function(err) {
-          return new Promise(function(resolve, reject) {
-            gm(imageFolder + fileName)
-              .setFormat('jpg')
-              .resize(width, height, '>')
-              .write(cacheFolder  + size + '/' + fileName, function(err){
-                if (err) return reject(err);
-                return resolve();
-              })
+        // if shoudnt be resized
+        // 1. check cached file
+        // 2. create new one if needed
 
-          })
-        })
-        .then(function() {
-          console.log('ImageServer:exists', _fileName);
-          fs.readFileAsync(_fileName)
-            .then(function(fileContent) {
-              resolve(fileContent);
+        var fileNameJpg = fileName.split('.');
+        fileNameJpg.length = fileNameJpg.length - 1;
+        fileNameJpg = fileNameJpg.join('.') + '.' + outputFormat;
+
+        var sourcePath = imageFolder + fileName;
+
+        path = cacheFolder; // 'cached/'
+        if(shoudBeResized) {
+            path +=  getCacheFolderName(normalizedSizeObject); // 'cached/100x100
+        }
+        path += '/' + fileNameJpg;
+
+        // check cached file
+        return fs.statAsync(path)
+            .then(function () {
+                return fs.readFileAsync(path)
             })
-        })
+            .catch(function () {
+                // cached file doesnt exist
+                console.log('ImageServer:File ' + path + ' didnt existed');
+                if(shoudBeResized) {
+                    // check if cache folder for this size exists
+                    return fs.statAsync(cacheFolder + getCacheFolderName(normalizedSizeObject))
+                        .catch(function () {
+                            // cache folder doesnt exists, creating
+                            console.log('ImageServer: folder didnt existed, created' + cacheFolder + size);
+                            return fs.mkdirAsync(cacheFolder + getCacheFolderName(normalizedSizeObject))
+                        })
+                        .then(function () {
+                            // creating the missing cached image
+                            return create(sourcePath, path, normalizedSizeObject)
+                                .then(function () {
+                                    console.log('ImageServer:File ' + path + ' didnt existed, created')
+                                })
+                        })
+                }
+                // creating the missing cached image
+                return create(sourcePath, path, normalizedSizeObject)
+                    .then(function () {
+                        console.log('ImageServer:File ' + path + ' didnt existed, created')
+                    })
+            })
+            .then(function () {
+                return fs.readFileAsync(path)
+            });
 
-    })
-  }
+        function create(originalPath, jpgPath, size) {
+            return new Promise(function (resolve, reject) {
+                gm(originalPath)
+                    .setFormat(outputFormat)
+                    .resize(size.width, size.height, '>')
+                    .write(jpgPath, function (err) {
+                        if (err) return reject(err);
+                        return resolve();
+                    })
+
+            })
+        }
+    }
+};
+function normalizeSizeObject(size) {
+    return {
+        width: size && size.width? parseInt(size.width) : null,
+        height: size && size.height? parseInt(size.height) : null
+    }
+}
+
+function getCacheFolderName(size) {
+
+    var width = size.width? size.width : "";
+    var height = size.height? size.height : "";
+
+    return width + 'x' + height;
+}
+function parseImageSize(size) {
+    if(!size) return null;
+
+    var splited = size.split('x');
+
+    // @example size = '100x', 'x600', '120x120'
+
+    var width = splited[0] !== ''? parseInt(splited[0]) : null;
+    var height = splited[1] !== ''? parseInt(splited[1]) : null;
+
+    return {
+        width: width,
+        height: height
+    }
 }
