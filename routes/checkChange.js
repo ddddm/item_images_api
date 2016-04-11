39d7a8208a5ad12c926f0df7a97f14e24cdf0c75
ddddm <m1290d@gmail.com>
2016-04-11 23:23:00 +0500

    Clean up old router

---
+++ routes/checkChange.js
@@ -0,0 +1,169 @@
+var Promise = require('bluebird');
+var _ = require('lodash');
+var express = require('express');
+var router = express.Router();
+
+var multer = require('multer');
+var upload = multer({dest: './uploads/'});
+
+var excelParser = require('../services/excelParser');
+var zipEntriesParser = require('../services/zipEntriesParser');
+var changeService = require('../services/changeService');
+var filenameWithoutExtension = require('../services/filenameWithoutExtension');
+
+var models = require('../models');
+
+router.route('/changes/check')
+    .post(
+    upload.fields([{name: 'excel'}, {name: 'zip'}]),
+    function (req, res) {
+
+        var validExtensions = ['jpg', 'jpeg', 'png', 'gif', 'bmp'];
+
+        if(_.isEmpty(req.files)) {
+            // handle situation where 1 or more files aint' attached to POST
+            return res.json(
+                {
+                    status:'error',
+                    message: 'Both zip and excel files must be attached to the request'
+                }
+            );
+        }
+
+        var unusedFiles = [],
+            unusedItems = [],
+            invalidItems = [],
+            validItems = [];
+
+        Promise.all([
+            excelParser.parse(req.files.excel[0].path),
+            zipEntriesParser.parse(req.files.zip[0].path)
+        ])
+            .spread(function (items, zipEntries) {
+                _.each(items, function processItem(item) {
+
+
+                    // Step 1: check this item is valid
+                    //
+
+                    if(!item.valid) return invalidItems.push(item);
+
+
+                    // Step 2:
+                    // check this item has a corresponding
+                    // image file attached
+
+                    var entry, entryName;
+
+                    if(item.image_file === 'no-picture.jpg' ||
+                        item.image_file === 'no-image.jpg') {
+                        console.log('Noimage file!');
+                        if(zipEntries[item.image_file]) {
+                            console.log('Noimage file exists',item.image_file );
+                        } else {
+                            console.log('Error! Noimage file doesn\'t exists',item.image_file );
+                        }
+                    }
+
+                    if(zipEntries[item.image_file]) {
+                        entry = zipEntries[item.image_file];
+                        entryName = item.image_file;
+                    }
+
+                    // if the image doesn't have the exact name
+                    // search for file using naming convention:
+                    // item code + validExtension
+
+                    function possibleFilename(code, ext) {
+                        return [code, ext].join('.');
+                    }
+
+                    _.each(validExtensions, function(ext) {
+                        var name = possibleFilename(item.code, ext);
+                        if(!entry && zipEntries[name]) {
+                            entry = zipEntries[name];
+                            entryName = name;
+                        }
+                    });
+
+                    if(!entry) {
+                        return unusedItems.push(item);
+                    }
+
+                    item.image_file = entryName;
+
+
+                    // Step 3: write corresponding file to disk
+                    //
+
+                    entry.stream(entry.name, function (err, stm) {
+                        if(err) console.error('Error extracting stream for file: ' + entry.name);
+                        zipEntriesParser.toDisk(item, stm);
+                    });
+
+
+                    // Step 4:
+                    // delete files from zip entries
+                    // to count unused images
+
+                    if(
+                        entryName !== 'no-picture.jpg' &&
+                        entryName !== 'no-image.jpg'
+                    ) {
+                        // we used this item
+                        delete zipEntries[entryName];
+                    }
+
+                    // store item for database insertion
+                    return validItems.push(item);
+                });
+
+                // do not need them any more
+                delete zipEntries['no-picture.jpg'];
+                delete zipEntries['no-image.jpg'];
+
+                // convert to array
+                unusedFiles = _.values(zipEntries);
+            })
+            .then(function () {
+                return res.json(
+                    {
+                        status:'ok',
+                        stats: {
+                            unusedFiles: unusedFiles.length,
+                            unusedItems: unusedItems.length,
+                            invalidItems: invalidItems.length,
+                            validItems: validItems.length
+                        },
+                        result: {
+                            unusedFiles: {
+                                total: unusedFiles.length,
+                                files: _.map(unusedFiles, function(file) {
+                                    return file.name;
+                                })
+                            },
+                            unusedItems: {
+                                total: unusedItems.length,
+                                items: _.map(unusedItems, function (item) {
+                                    return item.code + ', ' + item.image_file;
+                                })
+                            },
+                            invalidItems: {
+                                total: invalidItems.length,
+                                items:  _.map(invalidItems, function (item) {
+                                    return item.code + ', ' + item.image_file;
+                                })}
+                            //validItems: {total: validItems.length, items: validItems}
+                        }
+                        //https://jsbin.com/fesuduzavo/edit?html,js,console,output
+                    }
+                );
+            })
+            .catch(function (error) {
+                process.nextTick(function() { throw error; });
+                res.json({status: 'error'})
+            });
+    }
+);
+
+module.exports = router;
