43c7b54a94dee8b0b6057eeaca4e6077eb640b07
Dmitry Melnikov <m1290d@gmail.com>
2016-11-18 21:55:50 +0500

    delete unused method

--- services/changeImagesService.js
+++ services/changeImagesService.js
@@ -1,22 +1,13 @@
 "use strict";
 
 var fs = require("fs");
-var JSZip = require("jszip");
 var Promise = require('bluebird');
 
-var readFileAsync = Promise.promisify(fs.readFile);
 var writeFileAsync = Promise.promisify(fs.writeFile);
 
 var imagesDirectory = 'images';
 
 module.exports = {
-  listFiles: function(fileName) {
-    return readFileAsync(fileName)
-      .then(function (fileData) {
-        return new JSZip(fileData).files;
-      })
-  },
-  
   codeFromFilename: function(fileName) {
     if(!fileName) return false;
     var code = fileName.split('/');
